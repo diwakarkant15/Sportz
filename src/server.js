41f34304af9ef30ced78ec3757e2469d65abd29b
diwakarkant15 <diwakarkant15@gmail.com>
2026-01-31 18:12:08 +0530

    Applied database migrations

--- src/server.js
+++ src/server.js
@@ -1,12 +1,42 @@
 import express from 'express'
+import 'dotenv/config'
+import {Pool} from 'pg'
+import { drizzle } from 'drizzle-orm/neon-http';
 
 const app = express()
 
 const PORT = 3000
 
+const db = drizzle(process.env.DATABASE_URL)
+
+// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+// const pool = new Pool({
+//   host: PGHOST,
+//   database: PGDATABASE,
+//   username: PGUSER,
+//   password: PGPASSWORD,
+//   port: 5432,
+//   ssl: {
+//     require: true,
+//   },
+// });
+
+// async function getPgVersion(){
+//     const client = await pool.connect()
+//     try{
+//         const result = await client.query('SELECT version()');
+//         console.log(result.rows[0]);
+//     }finally{
+//         client.release();
+//     }
+// }
+
+
+
+
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+app.get('/', (req , res) =>{
     res.send("Hello from server")
 })
 
