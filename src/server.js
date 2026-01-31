2ae9c077b1f7013c57ce5c835241879cff196dca
diwakarkant15 <diwakarkant15@gmail.com>
2026-01-31 16:57:21 +0530

    initial commit with basic project setup

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send("Hello from server")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`);  
+})
